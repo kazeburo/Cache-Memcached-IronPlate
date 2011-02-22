935dceb087f1b59e7c6053d7929880d3b8cbdea0
Masahiro Nagano <kazeburo@gmail.com>
2011-02-22 18:04:39 +0900

    initial commit

---
+++ lib/Cache/Memcached/IronPlate.pm
@@ -0,0 +1,32 @@
+package Cache::Memcached::IronPlate;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Cache::Memcached::IronPlate -
+
+=head1 SYNOPSIS
+
+  use Cache::Memcached::IronPlate;
+
+=head1 DESCRIPTION
+
+Cache::Memcached::IronPlate is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
